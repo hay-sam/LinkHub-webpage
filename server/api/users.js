fce1d1b4c39567d60f352fba9ebad1649efa8eb0
Haley Sambursky <hsambursky46@gmail.com>
2019-11-17 23:54:08 -0500

    fix: removed unneccesary routes

--- server/api/users.js
+++ server/api/users.js
@@ -3,20 +3,6 @@ const router = require('express').Router()
 const {User, Post, Tag} = require('../db/models')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
-  }
-})
-
 router.get('/:userId/tags', async (req, res, next) => {
   const user = await User.findByPk(req.params.userId)
   const info = await user.getAllTags()
@@ -56,18 +42,6 @@ router.post('/:userId/posts', async (req, res, next) => {
       include: [{model: Tag}]
     })
     res.status(201).send(withTags)
-  } catch (err) {
-    console.error(err)
-    next(err)
-  }
-})
-
-router.get('/:userId/posts/:postId', async (req, res, next) => {
-  try {
-    const post = await Post.findByPk(req.params.postId, {
-      include: [{model: Tag}]
-    })
-    res.status(201).send(post)
   } catch (err) {
     console.error(err)
     next(err)
@@ -116,32 +90,6 @@ router.get('/:userId/posts/:postId/tags', async (req, res, next) => {
     const post = await Post.findByPk(req.params.postId)
     let tags = await post.getTags()
     res.status(201).send(tags)
-  } catch (err) {
-    console.error(err)
-    next(err)
-  }
-})
-
-router.post('/:userId/posts/:postId/tags', async (req, res, next) => {
-  try {
-    const post = await Post.findByPk(req.params.postId)
-    let tags = await req.body.tags.map(async tag => {
-      await Tag.findOne({where: {content: tag}})
-    })
-    post.addTags(tags)
-    res.sendStatus(201)
-  } catch (err) {
-    console.error(err)
-    next(err)
-  }
-})
-
-router.delete('/:userId/posts/:postId/tags/:tagId', async (req, res, next) => {
-  try {
-    const post = await Post.findByPk(req.params.postId)
-    let tag = await Tag.findByPk(req.params.tagId)
-    post.removeTag(tag)
-    res.sendStatus(201)
   } catch (err) {
     console.error(err)
     next(err)
