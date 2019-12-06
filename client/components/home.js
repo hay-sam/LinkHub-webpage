8de3e50dcdab0474e1a95b187e14fa811e8d630b
Haley Sambursky <hsambursky46@gmail.com>
2019-12-05 19:28:57 -0500

    style: created and styled home page

--- client/components/home.js
+++ client/components/home.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import {Link} from 'react-router-dom'
 
 const Home = props => {
-  return <div>Hi World</div>
+  return (
+    <div id="home">
+      <br />
+      <Typography variant="h3">Welcome to LinkHub!</Typography>
+      <br />
+      <div id="home-content">
+        <Typography variant="subtitle1">
+          Save links from across the web with the click of a button!
+        </Typography>
+        <Typography variant="subtitle1">
+          View your saved links in an easy to navigate interface!
+        </Typography>
+        <img id="home-logo" src="./LinkHub.png" />
+        <Typography variant="subtitle1">
+          Tag your links for easy lookup later!
+        </Typography>
+        <Typography variant="subtitle1">
+          <Link to="login">Log in</Link> or <Link to="signup">sign up</Link> to
+          start growing your Hub!
+        </Typography>
+      </div>
+    </div>
+  )
 }
 
 export default Home
